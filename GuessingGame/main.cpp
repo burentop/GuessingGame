87e6aef2ae9250898eebc8f4e24c4267fc0176ab
Brent Perry <burentoperii@gmail.com>
2016-07-08 15:08:43 -0500

    Initial commit of GuessingGame.

--- GuessingGame/main.cpp
+++ GuessingGame/main.cpp
@@ -6,10 +6,41 @@
 //  Copyright © 2016 Brent Perry. All rights reserved.
 //
 
-#include <iostream>
+#include "../../../std_lib_facilities.h"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+int main()
+{
+    char response;
+    int low = 1;
+    int high = 100;
+    bool solved = false;
+    
+    cout << "Please think of a number between 1 and 100.\n";
+    cout << "\n";
+    cout << "Got it? Good.\n";
+    
+    while (!solved) {
+        if (high - low == 1) {
+            cout << "Is the number you picked " << high << "? ('y' or 'n'): \n";
+            cin >> response;
+            if (response == 'y') {
+                cout << "You picked the number " << high << ".\n";
+                solved = true;
+            } else {
+                cout << "You picked the number " << low << ".\n";
+                solved = true;
+            }
+        } else {
+            cout << "Is the number you picked less than, or equal to, " << ((high + low) - 1) / 2 << "? ('y' or 'n'): \n";
+            cin >> response;
+            if (low == high) {
+                cout << "You picked the number " << low << ".\n";
+                solved = true;
+            } else if (response == 'y') {
+                high = ((high + low) - 1) / 2;
+            } else {
+                low = (((high + low) - 1) / 2) + 1;
+            }
+        }
+    }
 }
